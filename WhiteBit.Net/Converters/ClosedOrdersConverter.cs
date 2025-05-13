ba1ec82f180d2ca71af001a792181401b2af75a2
Jkorf <jankorf91@gmail.com>
2025-05-13 15:53:41 +0200

    Disable warning for AOT which is handled correctly

--- WhiteBit.Net/Converters/ClosedOrdersConverter.cs
+++ WhiteBit.Net/Converters/ClosedOrdersConverter.cs
@@ -10,6 +10,9 @@ using CryptoExchange.Net.Converters.SystemTextJson;
 
 namespace WhiteBit.Net.Converters
 {
+#pragma warning disable IL2026 // Members annotated with 'RequiresUnreferencedCodeAttribute' require dynamic access otherwise can break functionality when trimming application code
+#pragma warning disable IL3050 // Calling members annotated with 'RequiresDynamicCodeAttribute' may break functionality when AOT compiling.
+
     internal class ClosedOrdersConverter : JsonConverter<Dictionary<string, WhiteBitClosedOrder[]>>
     {
         private static readonly JsonSerializerOptions _deserializeOptions = SerializerOptions.WithConverters(new WhiteBitSourceGenerationContext());
