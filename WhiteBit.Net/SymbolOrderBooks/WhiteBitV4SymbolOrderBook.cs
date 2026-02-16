97f587b65ebe1f6858eecc48ccd6dd726b2bd62e
Jkorf <jankorf91@gmail.com>
2026-02-16 10:21:16 +0100

    Fixed SymbolOrderBook websocket subscription not getting closed if when waiting for initial data times out

--- WhiteBit.Net/SymbolOrderBooks/WhiteBitV4SymbolOrderBook.cs
+++ WhiteBit.Net/SymbolOrderBooks/WhiteBitV4SymbolOrderBook.cs
@@ -80,6 +80,9 @@ namespace WhiteBit.Net.SymbolOrderBooks
             Status = OrderBookStatus.Syncing;
 
             var setResult = await WaitForSetOrderBookAsync(_initialDataTimeout, ct).ConfigureAwait(false);
+            if (!setResult)
+                await result.Data.CloseAsync().ConfigureAwait(false);
+
             return setResult ? result : new CallResult<UpdateSubscription>(setResult.Error!);
         }
 
