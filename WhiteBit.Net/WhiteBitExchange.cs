1b8f0a33ea402ae32676f39a4e14a1910ee126cb
Jkorf <jankorf91@gmail.com>
2026-06-18 14:55:18 +0200

    wip

--- WhiteBit.Net/WhiteBitExchange.cs
+++ WhiteBit.Net/WhiteBitExchange.cs
@@ -102,7 +102,7 @@ namespace WhiteBit.Net
         /// <summary>
         /// Rate limiter configuration for the WhiteBit API
         /// </summary>
-        public static WhiteBitRateLimiters RateLimiter { get; } = new WhiteBitRateLimiters();
+        public static WhiteBitRateLimiters RateLimiter { get; set; } = new WhiteBitRateLimiters();
     }
 
     /// <summary>
@@ -121,13 +121,19 @@ namespace WhiteBit.Net
         public event Action<RateLimitUpdateEvent> RateLimitUpdated;
 
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-        internal WhiteBitRateLimiters()
+        /// <summary>
+        /// ctor
+        /// </summary>
+        public WhiteBitRateLimiters()
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         {
             Initialize();
         }
 
-        private void Initialize()
+        /// <summary>
+        /// Initialize the rate limits
+        /// </summary>
+        protected virtual void Initialize()
         {
             WhiteBit = new RateLimitGate("WhiteBit");
             WhiteBitSocket = new RateLimitGate("WhiteBit Socket")
