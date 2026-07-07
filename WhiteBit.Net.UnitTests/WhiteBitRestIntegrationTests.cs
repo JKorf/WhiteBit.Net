4593c65b881f225fdaf7d4fc907e7869dbc82686
Jkorf <jankorf91@gmail.com>
2026-07-07 11:17:23 +0200

    Updated CryptoExchange.Net to V12.0.2, Updated integration tests to new warning support

--- WhiteBit.Net.UnitTests/WhiteBitRestIntegrationTests.cs
+++ WhiteBit.Net.UnitTests/WhiteBitRestIntegrationTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Threading.Tasks;
 using CryptoExchange.Net.Authentication;
@@ -48,49 +49,61 @@ namespace WhiteBit.Net.UnitTests
         [Test]
         public async Task TestAccount()
         {
+            var warnings = new List<Exception>();
             await RunAndCheckResult(client => client.V4Api.Account.GetMainBalancesAsync(default), true);
-            await RunAndCheckResult(client => client.V4Api.Account.GetDepositAddressAsync("ETH", default, default), true);
+            //await RunAndCheckResult(warnings, client => client.V4Api.Account.GetDepositAddressAsync("ETH", "ERC20", default), true);
             await RunAndCheckResult(client => client.V4Api.Account.GetSpotBalancesAsync(default, default), true);
-            await RunAndCheckResult(client => client.V4Api.Account.GetDepositWithdrawalHistoryAsync(default, default, default, default, default, default, default, default, default), true);
-            await RunAndCheckResult(client => client.V4Api.Account.GetDepositWithdrawalSettingsAsync(default), true);
-            await RunAndCheckResult(client => client.V4Api.Account.GetMiningRewardHistoryAsync(default, default, default, default, default, default), true);
-            await RunAndCheckResult(client => client.V4Api.Account.GetCollateralBalancesAsync(default, default), true);
-            await RunAndCheckResult(client => client.V4Api.Account.GetCollateralBalanceSummaryAsync(default), true);
-            await RunAndCheckResult(client => client.V4Api.Account.GetCollateralAccountSummaryAsync(default), true);
-            await RunAndCheckResult(client => client.V4Api.Account.GetAccountFundingHistoryAsync("ETH_USDT", default, default, default, default, default), true);
+            await RunAndCheckResult(warnings, client => client.V4Api.Account.GetDepositWithdrawalHistoryAsync(default, default, default, default, default, default, default, default, default), true);
+            await RunAndCheckResult(warnings, client => client.V4Api.Account.GetDepositWithdrawalSettingsAsync(default), true);
+            await RunAndCheckResult(warnings, client => client.V4Api.Account.GetMiningRewardHistoryAsync(default, default, default, default, default, default), true);
+            await RunAndCheckResult(warnings, client => client.V4Api.Account.GetCollateralBalancesAsync(default, default), true);
+            await RunAndCheckResult(warnings, client => client.V4Api.Account.GetCollateralBalanceSummaryAsync(default), true);
+            await RunAndCheckResult(warnings, client => client.V4Api.Account.GetCollateralAccountSummaryAsync(default), true);
+            await RunAndCheckResult(warnings, client => client.V4Api.Account.GetAccountFundingHistoryAsync("ETH_PERP", default, default, default, default, default), true);
+            foreach (var warning in warnings)
+                Assert.Warn(warning.Message);
         }
 
         [Test]
         public async Task TestExchangeData()
         {
+            var warnings = new List<Exception>();
             await RunAndCheckResult(client => client.V4Api.ExchangeData.GetServerTimeAsync(default), false);
-            await RunAndCheckResult(client => client.V4Api.ExchangeData.GetSymbolsAsync(default), false);
-            await RunAndCheckResult(client => client.V4Api.ExchangeData.GetSystemStatusAsync(default), false);
+            await RunAndCheckResult(warnings, client => client.V4Api.ExchangeData.GetSymbolsAsync(default), false);
+            await RunAndCheckResult(warnings, client => client.V4Api.ExchangeData.GetSystemStatusAsync(default), false);
             await RunAndCheckResult(client => client.V4Api.ExchangeData.GetTickersAsync(default), false);
             await RunAndCheckResult(client => client.V4Api.ExchangeData.GetAssetsAsync(default), false);
-            await RunAndCheckResult(client => client.V4Api.ExchangeData.GetOrderBookAsync("ETH_USDT", default, default, default), false);
-            await RunAndCheckResult(client => client.V4Api.ExchangeData.GetRecentTradesAsync("ETH_USDT", default, default), false);
+            await RunAndCheckResult(warnings, client => client.V4Api.ExchangeData.GetOrderBookAsync("ETH_USDT", default, default, default), false);
+            await RunAndCheckResult(warnings, client => client.V4Api.ExchangeData.GetRecentTradesAsync("ETH_USDT", default, default), false);
             await RunAndCheckResult(client => client.V4Api.ExchangeData.GetDepositWithdrawalInfoAsync(default), false);
-            await RunAndCheckResult(client => client.V4Api.ExchangeData.GetCollateralSymbolsAsync(default), false);
-            await RunAndCheckResult(client => client.V4Api.ExchangeData.GetFuturesSymbolsAsync(default), false);
+            await RunAndCheckResult(warnings, client => client.V4Api.ExchangeData.GetCollateralSymbolsAsync(default), false, "result");
+            await RunAndCheckResult(warnings, client => client.V4Api.ExchangeData.GetFuturesSymbolsAsync(default), false, "result");
+            foreach (var warning in warnings)
+                Assert.Warn(warning.Message);
         }
 
         [Test]
         public async Task TestTrading()
         {
-            await RunAndCheckResult(client => client.V4Api.Trading.GetOpenOrdersAsync(default, default, default, default, default, default), true);
-            await RunAndCheckResult(client => client.V4Api.Trading.GetClosedOrdersAsync(default, default, default, default, default, default, default, default, default), true);
-            await RunAndCheckResult(client => client.V4Api.Trading.GetUserTradesAsync(default, default, default, default, default, default, default), true);
-            await RunAndCheckResult(client => client.V4Api.Trading.GetKillSwitchStatusAsync(default, default), true);
+            var warnings = new List<Exception>();
+            await RunAndCheckResult(warnings, client => client.V4Api.Trading.GetOpenOrdersAsync(default, default, default, default, default, default), true);
+            await RunAndCheckResult(warnings, client => client.V4Api.Trading.GetClosedOrdersAsync(default, default, default, default, default, default, default, default, default), true);
+            await RunAndCheckResult(warnings, client => client.V4Api.Trading.GetUserTradesAsync(default, default, default, default, default, default, default), true);
+            await RunAndCheckResult(warnings, client => client.V4Api.Trading.GetKillSwitchStatusAsync(default, default), true);
+            foreach (var warning in warnings)
+                Assert.Warn(warning.Message);
 
         }
 
         [Test]
         public async Task TestCollateralTrading()
         {
-            await RunAndCheckResult(client => client.V4Api.CollateralTrading.GetOpenPositionsAsync(default, default), true);
-            await RunAndCheckResult(client => client.V4Api.CollateralTrading.GetPositionHistoryAsync(default, default, default, default, default, default, default), true);
-            await RunAndCheckResult(client => client.V4Api.CollateralTrading.GetOpenConditionalOrdersAsync(default, default, default, default), true);
+            var warnings = new List<Exception>();
+            await RunAndCheckResult(warnings, client => client.V4Api.CollateralTrading.GetOpenPositionsAsync(default, default), true);
+            await RunAndCheckResult(warnings, client => client.V4Api.CollateralTrading.GetPositionHistoryAsync(default, default, default, default, default, default, default), true);
+            await RunAndCheckResult(warnings, client => client.V4Api.CollateralTrading.GetOpenConditionalOrdersAsync(default, default, default, default), true);
+            foreach (var warning in warnings)
+                Assert.Warn(warning.Message);
 
         }
 
