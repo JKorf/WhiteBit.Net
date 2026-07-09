dbad448b2fe6ac3877819083d86697474630bb86
Jkorf <jankorf91@gmail.com>
2026-07-09 16:05:03 +0200

    Updated CryptoExchange.Net to v12.1.0

--- WhiteBit.Net/WhiteBitTrackerFactory.cs
+++ WhiteBit.Net/WhiteBitTrackerFactory.cs
@@ -42,10 +42,10 @@ namespace WhiteBit.Net
         public bool CanCreateTradeTracker(SharedSymbol symbol) => true;
 
         /// <inheritdoc />
-        public IKlineTracker CreateKlineTracker(SharedSymbol symbol, SharedKlineInterval interval, int? limit = null, TimeSpan? period = null) => throw new NotImplementedException();
+        public IKlineTracker CreateKlineTracker(SharedSymbol symbol, SharedKlineInterval interval, int? limit = null, TimeSpan? period = null, ExchangeParameters? exchangeParameters = null) => throw new NotImplementedException();
 
         /// <inheritdoc />
-        public ITradeTracker CreateTradeTracker(SharedSymbol symbol, int? limit = null, TimeSpan? period = null)
+        public ITradeTracker CreateTradeTracker(SharedSymbol symbol, int? limit = null, TimeSpan? period = null, ExchangeParameters? exchangeParameters = null)
         {
             var restClient = _serviceProvider?.GetRequiredService<IWhiteBitRestClient>() ?? new WhiteBitRestClient();
             var socketClient = _serviceProvider?.GetRequiredService<IWhiteBitSocketClient>() ?? new WhiteBitSocketClient();
@@ -57,7 +57,8 @@ namespace WhiteBit.Net
                 socketClient.V4Api.SharedClient,
                 symbol,
                 limit,
-                period
+                period,
+                exchangeParameters
                 );
         }
 
